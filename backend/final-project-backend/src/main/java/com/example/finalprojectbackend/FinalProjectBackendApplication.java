6b3e18620b7b1c89b60d9cdcd55aa64975cd4868
sergio-mendez-telus <sergio.mendezgonzalez@telus.com>
2023-07-24 10:42:48 -0600

    initial commit

---
+++ backend/final-project-backend/src/main/java/com/example/finalprojectbackend/FinalProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectBackendApplication.class, args);
+    }
+
+}
