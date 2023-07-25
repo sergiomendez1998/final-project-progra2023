cf09e23a96f5d3d7a0d51d21606f37dd3878a64c
Silvia <silviacotzajay@gmail.com>
2023-07-25 15:46:52 -0600

    Prueba de pull request

--- backend/final-project-backend/src/main/java/com/example/finalprojectbackend/FinalProjectBackendApplication.java
+++ backend/final-project-backend/src/main/java/com/example/finalprojectbackend/FinalProjectBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalProjectBackendApplication {
-//   esto es una prueba
+
     public static void main(String[] args) {
         SpringApplication.run(FinalProjectBackendApplication.class, args);
     }
