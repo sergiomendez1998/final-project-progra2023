6de8b9d4ad3a7f149c173fa8afc6f6493ad23bdb
Sergio Santos Mendez <80726481+sergiomendez1998@users.noreply.github.com>
2023-08-16 11:55:03 -0600

    Delete backend/final-project-backend directory

--- backend/final-project-backend/src/main/java/com/example/finalprojectbackend/lab2you/db/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.finalprojectbackend.lab2you.db.repository;
-
-import com.example.finalprojectbackend.lab2you.db.model.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-    Role findByName(String name);
-}
